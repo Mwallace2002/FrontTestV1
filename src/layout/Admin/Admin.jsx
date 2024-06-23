969f981148d0cb990ff1e6983d697b41b7bc5a29
Mwallace2002 <Maxwallace2002@gmail.com>
2024-06-23 14:24:14 -0400

    Avance antes de Admin

--- src/layout/Admin/Admin.jsx
+++ src/layout/Admin/Admin.jsx
@@ -1,13 +1,69 @@
-// Admin.jsx
-import React from 'react';
+import React, { useState } from 'react';
+import './Admin.css'; // Asegúrate de importar el CSS aquí
 
-const Admin = () => {
+function Admin() {
+  const [tiempoEstancia, setTiempoEstancia] = useState('');
+  const [tiempoAdvertencia, setTiempoAdvertencia] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://apivercel-mwallace2002-max-wallaces-projects.vercel.app/api/parametros', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ TiempoEstancia: tiempoEstancia, TiempoAdvertencia: tiempoAdvertencia }),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setMessage(result.message);
+      } else {
+        setMessage('Error al actualizar parámetros');
+      }
+    } catch (error) {
+      console.error('Error al actualizar parámetros:', error);
+      setMessage('Error al actualizar parámetros');
+    }
+  };
+
   return (
-    <div>
-      <h1>Admin Page</h1>
-      <p>Welcome, Admin! Here you can manage everything.</p>
+    <div><Navbar />
+      <div className="admin-form-container">
+        <h1>Configuración de Parámetros</h1>
+        <form className="admin-form" onSubmit={handleSubmit}>
+          <label htmlFor="tiempoEstancia">Tiempo de Estancia</label>
+          <input
+            type="text"
+            id="tiempoEstancia"
+            value={tiempoEstancia}
+            onChange={(e) => setTiempoEstancia(e.target.value)}
+            required
+          />
+
+          <label htmlFor="tiempoAdvertencia">Tiempo para Advertencia</label>
+          <input
+            type="text"
+            id="tiempoAdvertencia"
+            value={tiempoAdvertencia}
+            onChange={(e) => setTiempoAdvertencia(e.target.value)}
+            required
+          />
+
+          <button type="submit">Actualizar Parámetros</button>
+        </form>
+
+        {message && (
+          <div className="message-container">
+            <p>{message}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
-};
+}
 
 export default Admin;
